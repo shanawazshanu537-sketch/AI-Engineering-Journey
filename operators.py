a796e5a6315bf62e7b312a90df5451a274544d71
shanawazshanu537-sketch <shanawazshanu537@gmail.com>
2026-08-13 16:20:51 +0530

    Lesson 4: strings

---
+++ operators.py
@@ -0,0 +1,30 @@
+a = 10
+b = 3
+
+# Arithmetic
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+# Comparison
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+
+# Logical
+x = True
+y = False
+print(x and y)
+print(x or y)
+print(not x)
+
+age = 20
+has_id = True
+
+can_enter = age >= 18 and has_id
+print(can_enter)   # True — both conditions are True
---
+++ variables1.py
@@ -0,0 +1,10 @@
+name = "Mohammad Shanawaz"
+age = 25
+is_learning = True
+
+print(name)
+print(age)
+print(is_learning)
+print(type(name))
+print(type(age))
+print(type(is_learning))
