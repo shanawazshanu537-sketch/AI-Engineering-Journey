325e2fc5216d33017d53d13bfd1357d0d208991c
shanawazshanu537-sketch <shanawazshanu537@gmail.com>
2026-08-13 16:17:30 +0530

    Lesson 3: input and type conversion

---
+++ input_basics.py
@@ -0,0 +1,9 @@
+name = input("what is your name: ")
+age = int(input("your age: "))
+
+fvrt_nmbr = float(input("favorite number: "))
+print(f"{name} + {age}")
+age += 5
+print(f"age will be in 5 years is {age}")
+fvrt_nmbr *= 2
+print(f"favorite number multiplied by 2 is {fvrt_nmbr}")
